2b66ec5ee051a0ddfb7e08db04237bbe24368d7d
marcelo@agenciasolo.com.br <50111485+marceloranieri@users.noreply.github.com>
2025-05-20 10:20:07 -0300

    chore: update shadcn components using new package name

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -3,6 +3,7 @@
 import * as React from "react"
 import * as AccordionPrimitive from "@radix-ui/react-accordion"
 import { ChevronDown } from "lucide-react"
+
 import { cn } from "@/lib/utils"
 
 const Accordion = AccordionPrimitive.Root
@@ -51,6 +52,7 @@ const AccordionContent = React.forwardRef<
     <div className={cn("pb-4 pt-0", className)}>{children}</div>
   </AccordionPrimitive.Content>
 ))
+
 AccordionContent.displayName = AccordionPrimitive.Content.displayName
 
 export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }

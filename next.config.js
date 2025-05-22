749359dd85b1482ba1467175c74431b2477070fa
marcelo@agenciasolo.com.br <50111485+marceloranieri@users.noreply.github.com>
2025-05-22 13:49:23 -0300

    Fix: Add webpack alias support for @ in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
   // Environment variables are stored in Vercel dashboard for production
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
 }
 
 module.exports = nextConfig 
